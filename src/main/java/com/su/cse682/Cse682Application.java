7229c9e81f9fdd2adcc639e3c985b1dfb6d83f67
FicusWang <srm602404151@gmail.com>
2018-08-27 16:19:02 -0400

    just init

---
+++ src/main/java/com/su/cse682/Cse682Application.java
@@ -0,0 +1,12 @@
+package com.su.cse682;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cse682Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cse682Application.class, args);
+	}
+}
---
+++ src/test/java/com/su/cse682/Cse682ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.su.cse682;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cse682ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
